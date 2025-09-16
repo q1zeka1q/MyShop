ef0ef2c72b2d6fbf9967931e7c9f10653d05a06b
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-09-16 19:42:45 +0300

    Kindergarden oli tehtud

---
+++ ShopTARgv24/ShopTARgv24.Core/Domain/Kindergarden.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARgv24.Core.Domain
+{
+    public class Kindergarden
+    {
+        public Guid? Id { get; set; }
+        public string? GroupName { get; set; }
+        public int? ChildrenCount { get; set; }
+        public string? KindergardenName { get; set; }
+        public string? TeacherName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        //Teha uus branch nimega Kindergarden
+        //Teha uus CRUD lasamine, vaatamine, uuendamine ja andmete kustutamine.
+        //Teemaks on Kindergarden
+        //Muutujateks on ID, GroupName, ChildrenCount, KindergardenName, TeacherName, CreatedAt, UpdatedAt
+        //Too on hindeline. Too panna githubi ja link saata emailile. Tunnis naitad ette
+    }
+}
