16201dcb1b851395913bc5addf97e86b95ae919a
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-09-30 14:48:55 +0300

    index

---
+++ ShopTARgv24/ShopTARgv24/Controllers/RealEstateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopTARgv24.Controllers
+{
+    public class RealEstateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
