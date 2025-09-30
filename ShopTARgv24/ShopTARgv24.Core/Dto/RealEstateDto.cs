d7f2230abfff3c4885333d56154fc5633447507f
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-09-30 14:07:09 +0300

    realestatedto

---
+++ ShopTARgv24/ShopTARgv24.Core/Dto/RealEstateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopTARgv24.Core.Dto
+{
+    public class RealEstateDto
+    {
+        public Guid? Id { get; set; }
+        public double? Area { get; set; }
+        public string? Location { get; set; }
+        public int? RoomNumber { get; set; }
+        public string? BuildingType { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
