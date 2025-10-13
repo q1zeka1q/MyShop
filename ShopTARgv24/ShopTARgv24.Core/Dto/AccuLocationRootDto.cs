2ef805a88ac58e376b300b87aa2a7ae26f39170d
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-10-13 16:42:27 +0300

    tund 6

--- ShopTARgv24/ShopTARgv24.Core/Dto/AccuLocationRootDto.cs
+++ ShopTARgv24/ShopTARgv24.Core/Dto/AccuLocationRootDto.cs
@@ -3,6 +3,33 @@ namespace ShopTARgv24.Core.Dto
 {
     public class AccuLocationRootDto
     {
+        public string LocalObservationDateTime { get; set; }
+        public string EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public string PrecipitationType { get; set; }
+        public bool IsDayTime { get; set; }
+
+        public class TemperatureInfo
+        {
+            public MetricTemperature Metric { get; set; }
+            public ImperialTemperature Imperial { get; set; }
+        }
+
+        public class MetricTemperature
+        {
+            public double Value { get; set; }
+            public string Unit { get; set; }
+            public int UnitType { get; set; }
+        }
+
+        public class ImperialTemperature
+        {
+            public double Value { get; set; }
+            public string Unit { get; set; }
+            public int UnitType { get; set; }
+        }
 
     }
 }
